12719b36fcb2c0813a3880ed5eae9207994ecd17
LiquidFiend <adamlatimer@gmail.com>
2017-03-13 14:09:43 -0400

    Alpha
    Pick from file, or drag and drop filter

---
+++ MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ChangeLootFilterSize
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPickFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNewFilterName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCreateNewFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPickFile
+            // 
+            this.btnPickFile.Location = new System.Drawing.Point(187, 47);
+            this.btnPickFile.Name = "btnPickFile";
+            this.btnPickFile.Size = new System.Drawing.Size(33, 20);
+            this.btnPickFile.TabIndex = 0;
+            this.btnPickFile.Text = "...";
+            this.btnPickFile.UseVisualStyleBackColor = true;
+            this.btnPickFile.Click += new System.EventHandler(this.btnPickFile_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(28, 47);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(153, 20);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(33, 95);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            27,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            27,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(142, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Filter:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Amount to increase or decrease by:";
+            // 
+            // txtNewFilterName
+            // 
+            this.txtNewFilterName.Location = new System.Drawing.Point(28, 134);
+            this.txtNewFilterName.Name = "txtNewFilterName";
+            this.txtNewFilterName.Size = new System.Drawing.Size(153, 20);
+            this.txtNewFilterName.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "New filter name:";
+            // 
+            // btnCreateNewFile
+            // 
+            this.btnCreateNewFile.Location = new System.Drawing.Point(54, 160);
+            this.btnCreateNewFile.Name = "btnCreateNewFile";
+            this.btnCreateNewFile.Size = new System.Drawing.Size(100, 20);
+            this.btnCreateNewFile.TabIndex = 9;
+            this.btnCreateNewFile.Text = "Create New Filter";
+            this.btnCreateNewFile.UseVisualStyleBackColor = true;
+            this.btnCreateNewFile.Click += new System.EventHandler(this.btnCreateNewFile_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(230, 197);
+            this.Controls.Add(this.btnCreateNewFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNewFilterName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnPickFile);
+            this.Name = "MainForm";
+            this.Text = "Change Loot Filter Size";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPickFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNewFilterName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCreateNewFile;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeLootFilterSize
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.AllowDrop = true;
+            this.DragEnter += MainForm_DragEnter;
+            this.DragDrop += MainForm_DragDrop;
+        }
+
+        void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) 
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length > 0 && Path.GetExtension(files[0]) == "filter")
+                SetFileName(files[0]);
+        }
+
+       
+
+        private const int MINSIZE = 18;
+        private const int MAXSIZE = 45;
+
+        private void btnCreateNewFile_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(txtFilePath.Text))
+            {
+                MessageBox.Show("Filter doesn't exist");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(txtNewFilterName.Text) || txtNewFilterName.Text == Path.GetFileNameWithoutExtension(txtFilePath.Text))
+            {
+                MessageBox.Show("File must have a new name, and cannot be blank");
+                return;
+            }
+
+            string file = txtFilePath.Text;
+            string path = Path.GetDirectoryName(file);
+
+            StringBuilder modifiedFilter = new StringBuilder();
+
+            using (StreamReader read = new StreamReader(file))
+            {
+                while (!read.EndOfStream)
+                {
+
+                    string line = read.ReadLine();
+                    string trimmedLine = line.Trim();
+
+                    if (trimmedLine.StartsWith("SetFontSize"))
+                    {
+                        int fontSize = Convert.ToInt16(trimmedLine.Substring(trimmedLine.Length - 2));
+                        int modifiedSize = fontSize;
+                        modifiedSize += (int)numericUpDown1.Value;
+
+                        modifiedSize = modifiedSize > MAXSIZE ? MAXSIZE : modifiedSize;
+                        modifiedSize = modifiedSize < MINSIZE ? MINSIZE : modifiedSize;
+
+                        line = line.Replace(fontSize.ToString(), modifiedSize.ToString());
+                    }
+
+                    modifiedFilter.AppendLine(line);
+                }
+
+                read.Close();
+            }
+
+
+            using (StreamWriter writer = new StreamWriter(Path.Combine(path, txtNewFilterName.Text + ".filter")))
+            {
+                writer.Write(modifiedFilter.ToString());
+                writer.Close();
+            }
+        }
+
+        private void btnPickFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "POE Filters (*.filter)|*.filter";
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                SetFileName(dialog.FileName);
+        }
+
+        private void SetFileName(string fileName)
+        {
+            txtFilePath.Text = fileName;
+            txtNewFilterName.Text = Path.GetFileNameWithoutExtension(fileName);
+        }
+    }
+}
